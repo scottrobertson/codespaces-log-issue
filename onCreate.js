8d9b6f9546596d076f7f36a75c6aad277d3548c6
Scott Robertson <scottrobertson@users.noreply.github.com>
2021-08-25 00:09:47 +0000

    Show total count

--- onCreate.js
+++ onCreate.js
@@ -1,3 +1,7 @@
-for (let i = 0; i < 100000; i++) {
-  console.log(`Line ${i} with lots of text lkdgnmlsdknglsdngsd gsdklj gsdklj gkljds gkjds glkjds agkj dskagj dslkgj sdalkagj sdklgj sd`);
+const totalCount = 100000;
+
+for (let i = 0; i < totalCount; i++) {
+  console.log(
+    `Line ${i}/${totalCount} with lots of text lkdgnmlsdknglsdngsd gsdklj gsdklj gkljds gkjds glkjds agkj dskagj dslkgj sdalkagj sdklgj sd`
+  );
 }
